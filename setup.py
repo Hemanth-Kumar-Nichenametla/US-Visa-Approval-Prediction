fc0fd0210c04c9111afc6543f1880b7781442167
Hemanth-Kumar-Nichenametla <hnichena@cougarnet.uh.edu>
2024-03-06 14:32:26 -0600

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Hemanth",
+    author_email="hemanth.vgf59@gmail.com",
+    packages=find_packages()
+)
