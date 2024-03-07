78eae726736284a7710d964bbca04fd242509986
Hemanth-Kumar-Nichenametla <hnichena@cougarnet.uh.edu>
2024-03-06 23:34:06 -0600

    Exception Code Added

--- demo.py
+++ demo.py
@@ -1,3 +1,11 @@
 from us_visa.logger import logging
 
-logging.info("Welcome to our Custom log")
+from us_visa.exception import USvisaException
+import sys
+
+try:
+    a=1/"10"
+except Exception as e:
+    raise USvisaException(e,sys) from e
+
+#logging.info("Welcome to our Custom log")
